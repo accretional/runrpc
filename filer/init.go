2f1b0d6d98665c48cf4921d9f474425dd4ed274f
Fred Weitendorf <37577626+fredxfred@users.noreply.github.com>
2026-03-11 10:14:36 -0700

    Create init.go

---
+++ filer/init.go
@@ -0,0 +1,29 @@
+package filer
+
+import (
+  "embed"
+  "log"
+  "sync"
+
+  "github.com/google/uuid"
+)
+
+//go:embed *.proto
+var ProtoFiles embed.FS
+
+// TODO iterate over ProtoFiles and: 
+// 1. fill user_types with ("system", map<filename, filedescriptorproto>)
+// 2. fill user_protos with ("system/typename", descriptorproto)
+// 3. fill user_rpc with ("system/service", servicedescriptorproto) and ("system/service.method", methoddescriptorproto)
+// 4. fill shared_types with all system FileDescriptorProtos merged into one
+var user_proto sync.Map
+var user_service sync.Map
+var user_rpc sync.Map
+var shared_types FileDesciptorProto
+
+var server_id = "default"
+
+func init() {
+  // replace with standard library uuid when ready https://github.com/golang/go/issues/62026
+  server_id = uuid.New().String()
+}
